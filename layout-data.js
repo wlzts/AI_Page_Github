d1fc7513ffaa3d29120b280de49450495cbdc666
wlzts <141948869+wlzts@users.noreply.github.com>
2026-08-14 15:11:18 +0800

    更新 V10 可视化页面布局

--- layout-data.js
+++ layout-data.js
@@ -10,18 +10,97 @@ window.AIPageLayout = {
     "eat-what",
     "wordvideo",
     "do-divination",
-    "white-house-master",
     "playable-resume",
+    "white-house-master",
     "Lyrictype Project"
   ],
   "settings": {
-    "desktop": { "columns": 12, "gap": 14, "rowHeight": 120 },
-    "tablet":  { "columns": 6,  "gap": 12, "rowHeight": 115 },
-    "mobile":  { "columns": 1,  "gap": 12, "rowHeight": 110 }
+    "desktop": {
+      "columns": 12,
+      "gap": 14,
+      "rowHeight": 120
+    },
+    "tablet": {
+      "columns": 6,
+      "gap": 12,
+      "rowHeight": 115
+    },
+    "mobile": {
+      "columns": 1,
+      "gap": 12,
+      "rowHeight": 110
+    }
   },
   "style": {
     "cardRadius": 24,
-    "cardPadding": 22
+    "cardPadding": 18
   },
-  "cards": {}
+  "cards": {
+    "wash-hair": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "basketball-position-matching": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "fit-meal": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "gomoku": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "basketball-shot": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "eat-what": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "wordvideo": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "do-divination": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "white-house-master": {
+      "desktop": {
+        "colSpan": 6,
+        "rowSpan": 2
+      }
+    },
+    "playable-resume": {
+      "desktop": {
+        "colSpan": 12,
+        "rowSpan": 2
+      }
+    },
+    "Lyrictype Project": {
+      "desktop": {
+        "colSpan": 12,
+        "rowSpan": 3
+      }
+    }
+  }
 };
